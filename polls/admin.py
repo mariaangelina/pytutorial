feb76ed0d28228fe8b11a00d9816d426bc2df32f
Maria Angelina <maria.angelina.b@gmail.com>
2018-10-29 18:03:43 +0700

    Customize the admin form https://docs.djangoproject.com/en/2.1/intro/tutorial07/

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # detail page
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    # listing / grid
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # filter on listing page
+    list_filter = ['pub_date']
+    # search bar
+    search_fields = ['question_text', 'pub_date']
+
+admin.site.register(Question, QuestionAdmin)
